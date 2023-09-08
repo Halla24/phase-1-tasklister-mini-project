b93b3a0a45af4242432337f614be250f5e0137b3
halla <aniabisso.16@gmail.com>
2023-09-09 00:15:15 +0100

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+
+    const taskDescription = document.getElementById("new-task-description").value;
+
+    const taskItem = document.createElement("li");
+    taskItem.textContent = taskDescription;
+
+    taskList.appendChild(taskItem);
+
+    document.getElementById("new-task-description").value = "";
+  });
 });
+
+
